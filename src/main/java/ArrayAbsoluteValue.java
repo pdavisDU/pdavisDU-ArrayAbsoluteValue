de4778abb7b21709d3c06922b15fdcc8e2bfd757
Paul Davis <pdavisDU@gmail.com>
2023-09-04 18:24:51 +0000

    passed all testing

--- src/main/java/ArrayAbsoluteValue.java
+++ src/main/java/ArrayAbsoluteValue.java
@@ -9,6 +9,11 @@ public class ArrayAbsoluteValue {
      * @return the absolute value array of nums.
      */
     public int[] getArrayAbs(int[] nums){
-        return null;
+        int[] absArray = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            absArray[i] = Math.abs(nums[i]);
+        }
+
+        return absArray;
     }
 }
